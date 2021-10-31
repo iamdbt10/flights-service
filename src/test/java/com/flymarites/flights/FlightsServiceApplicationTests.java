c2b76848a4eeaa10821227db354eeaf653092eef
Dexter B. Turno <dbturno@gmail.com>
2021-11-01 07:14:44 +0800

    Initial Commit

---
+++ src/test/java/com/flymarites/flights/FlightsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flymarites.flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
